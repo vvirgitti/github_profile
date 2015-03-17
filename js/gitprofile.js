d13fdcdb74f0fb970c2be5197a0a043c45ecb1ad
Vanessa <virgitti@gmail.com>
2015-03-17 16:26:53 +0000

    added git profile.js

---
+++ js/gitprofile.js
@@ -0,0 +1,19 @@
+$(document).ready(function() {
+
+  $('.gitprofile').on('submit', function(e) {
+
+    e.preventDefault();
+
+    var url = 'https://api.github.com/users/' + $('input.username').val();
+
+    var template = $('template').html();
+
+    var info = $.get(url, function(info) {
+      $('.container').prepend(Mustache.render(template, info));
+    }).fail(function() {
+      $('.container').prepend("User not found")
+    }).always(function() {
+      $('input.username').val('');
+      });
+  });
+});
