f63c4a8f29df67cf0543c0a7d54f2a7ecf15b222
Vanessa <virgitti@gmail.com>
2015-03-18 12:00:08 +0000

    defined searchResult scope

--- js/gitUserSearchController.js
+++ js/gitUserSearchController.js
@@ -1,3 +1,16 @@
 githubUserSearch.controller('GitUserSearchController', function($scope) {
-
+  $scope.searchResult = {
+  "items": [
+    {
+      "login": "tansaku",
+      "avatar_url": "https://avatars.githubusercontent.com/u/30216?v=3",
+      "html_url": "https://github.com/tansaku"
+    },
+    {
+      "login": "stephenlloyd",
+      "avatar_url": "https://avatars.githubusercontent.com/u/196474?v=3",
+      "html_url": "https://github.com/stephenlloyd"
+    }
+  ]
+};
 });
--- spec/gitUserSearchController.spec.js
+++ spec/gitUserSearchController.spec.js
@@ -14,5 +14,25 @@ describe('GitUserSearchController', function() {
     expect(scope.searchResult).toBeUndefined();
     expect(scope.searchTerm).toBeUndefined();
   });
-  
+
+  describe('when searching for a user', function() {
+
+    var items = [
+      {
+        "login": "tansaku",
+        "avatar_url": "https://avatars.githubusercontent.com/u/30216?v=3",
+        "html_url": "https://github.com/tansaku"
+      },
+      {
+        "login": "stephenlloyd",
+        "avatar_url": "https://avatars.githubusercontent.com/u/196474?v=3",
+        "html_url": "https://github.com/stephenlloyd"
+      }
+    ];
+
+    it('displays search results', function() {
+      expect(scope.searchResult.items).toEqual(items);
+    });
+  });
+
 });
