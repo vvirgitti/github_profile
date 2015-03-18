02169f860d6673c107e62f89209c684bf18bb509
Julian Veling <julianveling@Julians-MacBook-Pro.local>
2015-03-18 18:27:12 +0000

    added getting followers and public repos

--- js/gitUserSearchController.js
+++ js/gitUserSearchController.js
@@ -1,10 +1,19 @@
-githubUserSearch.controller('GitUserSearchController', function($scope, $resource) {
-
-  var searchResource = $resource('https://api.github.com/search/users?access_token=d1ba947cc408fd93882dd477daffe48284fe1a31');
+githubUserSearch.controller('GitUserSearchController', function($scope, $resource, $http) {
+  var searchResource = $resource('https://api.github.com/search/users');
 
   $scope.doSearch = function (){
+
     $scope.searchResult = searchResource.get({
-      q: $scope.searchTerm
+      access_params: "d1ba947cc408fd93882dd477daffe48284fe1a31", 
+      q: $scope.searchTerm,
+    }, function(){
+      var name = 'https://api.github.com/users/' + $scope.searchTerm;
+      $http.get(name).
+        success(function(data){
+          $scope.individualData = data     
+        });
     });
+
   };
+
 });
